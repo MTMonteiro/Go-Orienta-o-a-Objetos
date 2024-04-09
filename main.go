c94b8375dce3d8bb8f34187518110bf7b4ae4d3a
Matheus Monteiro Alves <matheus.alves@intelie.com.br>
2024-04-09 00:48:56 -0300

    comparação

--- main.go
+++ main.go
@@ -10,5 +10,27 @@ type ContaCorrente struct {
 }
 
 func main() {
+	conta1 := ContaCorrente{titular: "Carlos", numeroAgencia: 01, numeroConta: 111, saldo: 0}
+	conta2 := ContaCorrente{"Bruna", 01, 222, 5_000}
+	conta22 := ContaCorrente{"Bruna", 01, 222, 5_000}
+
 	fmt.Println(ContaCorrente{})
+	fmt.Println(conta1)
+	fmt.Println(conta2)
+
+	var conta3 *ContaCorrente
+	conta3 = new(ContaCorrente)
+	// var conta3 = new(ContaCorrente)
+	conta3.titular = "Pedrinho"
+	fmt.Println(conta3)
+
+	var conta33 *ContaCorrente
+	conta3 = new(ContaCorrente)
+	// var conta3 = new(ContaCorrente)
+	conta3.titular = "Pedrinho"
+	fmt.Println(conta3)
+	fmt.Println(conta3 == conta33)   // false compara endereços de memoria
+	fmt.Println(*conta3 == *conta33) // true
+
+	fmt.Println(conta2 == conta22) // no go vc consegue comparar objetos true
 }
